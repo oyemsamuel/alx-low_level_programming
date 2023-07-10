8cf391f01fdfc2ab8fd46872407a1c49df8e3393
oyemsamuel <oyemsamuel20@gmail.com>
2023-07-10 03:26:48 +0000

    complete task 2 for the month

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *  _memset - function that fills memory with a constant byte.
+ * @s: character.
+ * @b: character.
+ * @n: unsigned integer.
+ * Return: s.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int a;
+
+for (a = 0; a < n; a++)
+{
+	s[a] = b;
+}
+return (s);
+}
